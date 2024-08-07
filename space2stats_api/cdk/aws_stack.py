5355470f4de6453fa562aa10e18fbcbc643adb68
Zachary Deziel <zachary.deziel@gmail.com>
2024-08-07 13:17:56 -0700

    Ruff formatting

--- space2stats_api/cdk/aws_stack.py
+++ space2stats_api/cdk/aws_stack.py
@@ -4,11 +4,12 @@ from aws_cdk import (
     aws_apigatewayv2_integrations as integrations,
     aws_lambda as _lambda,
     aws_certificatemanager as acm,
-    Duration
+    Duration,
 )
 from aws_cdk.aws_lambda_python_alpha import PythonFunction
 from constructs import Construct
 from settings import AppSettings, DeploymentSettings
+
 
 class Space2StatsStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
@@ -18,35 +19,40 @@ class Space2StatsStack(Stack):
         deployment_settings = DeploymentSettings(_env_file="./aws_deployment.env")
 
         lambda_function = PythonFunction(
-            self, "Space2StatsFunction",
+            self,
+            "Space2StatsFunction",
             entry="../src",
             runtime=_lambda.Runtime.PYTHON_3_11,
             index="app/main.py",
             timeout=Duration.seconds(120),
             handler="handler",
             environment=app_settings.model_dump(),
-            memory_size=1024
+            memory_size=1024,
         )
 
-        certificate = acm.Certificate.from_certificate_arn(self, "Certificate", deployment_settings.CDK_CERTIFICATE_ARN)
+        certificate = acm.Certificate.from_certificate_arn(
+            self, "Certificate", deployment_settings.CDK_CERTIFICATE_ARN
+        )
 
         domain_name = apigatewayv2.DomainName(
-            self, "DomainName",
+            self,
+            "DomainName",
             domain_name=deployment_settings.CDK_DOMAIN_NAME,
-            certificate=certificate
+            certificate=certificate,
         )
 
         http_api = apigatewayv2.HttpApi(
-            self, "Space2StatsHttpApi",
+            self,
+            "Space2StatsHttpApi",
             default_integration=integrations.HttpLambdaIntegration(
-                "LambdaIntegration",
-                handler=lambda_function
-            )
+                "LambdaIntegration", handler=lambda_function
+            ),
         )
 
         apigatewayv2.ApiMapping(
-            self, "ApiMapping",
+            self,
+            "ApiMapping",
             api=http_api,
             domain_name=domain_name,
-            stage=http_api.default_stage
+            stage=http_api.default_stage,
         )
--- space2stats_api/src/app/main.py
+++ space2stats_api/src/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -16,8 +16,10 @@ app.add_middleware(
 
 app.include_router(api.router)
 
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Space2Stats!"}
+
 
 handler = Mangum(app)
