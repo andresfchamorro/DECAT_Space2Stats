60957139e1b2da31c2b3d047be4baa60352800d7
Zachary Deziel <zachary.deziel@gmail.com>
2024-10-04 09:16:52 -0700

    wip migrate to container based image

--- space2stats_api/cdk/aws_stack.py
+++ space2stats_api/cdk/aws_stack.py
@@ -2,9 +2,9 @@ from aws_cdk import Duration, Stack
 from aws_cdk import aws_apigatewayv2 as apigatewayv2
 from aws_cdk import aws_apigatewayv2_integrations as integrations
 from aws_cdk import aws_certificatemanager as acm
+from aws_cdk import aws_ecr as ecr
 from aws_cdk import aws_lambda as _lambda
 from aws_cdk import aws_s3 as s3
-from aws_cdk.aws_lambda_python_alpha import PythonFunction
 from constructs import Construct
 from settings import AppSettings, DeploymentSettings
 
@@ -22,19 +22,23 @@ class Space2StatsStack(Stack):
             lifecycle_rules=[s3.LifecycleRule(expiration=Duration.days(1))],
         )
 
-        lambda_function = PythonFunction(
+        lambda_function = _lambda.DockerImageFunction(
             self,
-            "Space2StatsFunction",
-            entry="../src",
-            runtime=_lambda.Runtime.PYTHON_3_11,
-            index="space2stats/api/handler.py",
+            "Space2StatsFunc",
+            code=_lambda.DockerImageCode.from_ecr(
+                repository=ecr.Repository.from_repository_attributes(
+                    self,
+                    "Space2StatsRepo",
+                    repository_arn="arn:aws:ecr:us-east-1:017820688988:repository/space2stats",  # Correct ARN
+                    repository_name="space2stats",  # Correct repository name
+                )
+            ),
             timeout=Duration.seconds(120),
-            handler="handler",
+            memory_size=1024,
             environment={
                 "S3_BUCKET_NAME": bucket.bucket_name,
                 **app_settings.model_dump(),
             },
-            memory_size=1024,
         )
 
         bucket.grant_read_write(lambda_function)
